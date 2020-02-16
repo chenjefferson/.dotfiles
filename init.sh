201556f21c4a9ae76bb6a848aefe5bf9eec5c3f4
Jefferson William Chen <jeffwilliamchen@ieee.org>
2020-02-16 02:13:45 -0800

    added vim (plugins and dir)

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@
 #!/bin/bash
 
 FILES="vimrc gitconfig tmux.conf shell_config"
+FOLDERS="vim"
 
 # prompt user for consent to create links
 echo    "this script should only be run in the dotfiles directory."
@@ -23,5 +24,17 @@ do
         ln -s "${PWD}"/"${file}" "${HOME}"/."${file}"
     else
         echo "FAILED: ~/.${file} already exists"
+    fi
+done
+
+# link all folders in "${FOLDERS}"
+for folder in $FOLDERS
+do
+    if [ ! -e ~/."${folder}" ]
+    then
+        echo "linking ${folder} as ~/.${folder}"
+        ln -s "${PWD}"/"${folder}" "${HOME}"/."${folder}"
+    else
+        echo "FAILED: ~/.${folder} already exists"
     fi
 done
